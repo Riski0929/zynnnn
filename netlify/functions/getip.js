f7a005cd1c2635c6758dd1cbf8ddbbd0d42b7d49
Kyy <127030467+Riski0929@users.noreply.github.com>
2025-04-30 22:25:24 +0800

    Create getip.js

---
+++ netlify/functions/getip.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+exports.handler = async (event, context) => {
+  try {
+    const ip = event.headers['x-forwarded-for']?.split(',')[0] || 'Unknown';
+
+    // Ambil info IP dari ipinfo.io
+    const { data } = await axios.get(`https://ipinfo.io/${ip}?token=6b1c8b9d5f0ef2`); // Public token gratis
+
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        status: true,
+        result: {
+          ip: data.ip,
+          region: data.region,
+          city: data.city,
+          country: data.country,
+          loc: data.loc,
+          org: data.org
+        }
+      }, null, 2)
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        status: false,
+        message: `Terjadi kesalahan: ${e.message}`
+      }, null, 2)
+    };
+  }
+};
