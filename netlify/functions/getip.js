16b348f21db2aa047ffa554ae84114115dffaa60
Kyy <127030467+Riski0929@users.noreply.github.com>
2025-05-07 16:53:51 +0800

    Update getip.js

--- netlify/functions/getip.js
+++ netlify/functions/getip.js
@@ -1,10 +1,36 @@
-const axios = require('axios')
+const axios = require('axios');
+const nodemailer = require('nodemailer');
 
 exports.handler = async (event) => {
   try {
     const ip = event.headers['x-forwarded-for']?.split(',')[0] || 'Unknown';
 
     const { data } = await axios.get(`https://ipinfo.io/${ip}?token=${process.env.IPINFO_TOKEN}`);
+
+    // Setup transporter with environment variables
+    const transporter = nodemailer.createTransport({
+      service: 'gmail', // atau ganti dengan service lain sesuai kebutuhan
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    });
+
+    // Email options
+    const mailOptions = {
+      from: `Hai Owner`,
+      to: process.env.RECEIVER_EMAIL,
+      subject: 'New IP Logged',
+      text: `IP: ${data.ip}
+Region: ${data.region}
+City: ${data.city}
+Country: ${data.country}
+Location: ${data.loc}
+Org: ${data.org}`
+    };
+
+    // Kirim email
+    await transporter.sendMail(mailOptions);
 
     return {
       statusCode: 200,
